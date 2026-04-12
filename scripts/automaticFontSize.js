e68576b967e029f33f21bba2bcef3f0885d28d42
JElouann <eljouenne@gmail.com>
2026-04-12 12:40:54 +0200

    added error protections to tiles query selector

--- scripts/automaticFontSize.js
+++ scripts/automaticFontSize.js
@@ -9,6 +9,10 @@ const getFontSize = (textLength, gridColumnCount) => {
 
 const titles = document.querySelectorAll('.project-title')
 
+if (!titles) {
+  console.warn("Titles .project-title introuvables");
+  return; // stop proprement
+}
 const gridComputedStyle = window.getComputedStyle(document.getElementById('project-grid'));
 const gridColumnCount = gridComputedStyle.getPropertyValue("grid-template-columns").split(" ").length;
 
